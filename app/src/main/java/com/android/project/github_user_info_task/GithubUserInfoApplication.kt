0c823c3780be0267ca4a85b6546d18943a949cc6
welsen <welsen9595@gmail.com>
2021-08-18 00:37:33 +0800

    feat(Initial commit): Initial commit

---
+++ app/src/main/java/com/android/project/github_user_info_task/GithubUserInfoApplication.kt
@@ -0,0 +1,9 @@
+package com.android.project.github_user_info_task
+
+import android.app.Application
+
+class GithubUserInfoApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/android/project/github_user_info_task/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.android.project.github_user_info_task.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.project.github_user_info_task.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
