55fc8976c79fb4b284d02d0edf2e35a6509b8db1
welsen <welsen9595@gmail.com>
2021-08-19 11:59:01 +0800

    feat(GitUserInfo): Code refactor

--- app/src/main/java/com/android/project/github_user_info_task/data/repository/user_info/GetUserInfoRepository.kt
+++ app/src/main/java/com/android/project/github_user_info_task/data/repository/user_info/GetUserInfoRepository.kt
@@ -2,7 +2,6 @@ package com.android.project.github_user_info_task.data.repository.user_info
 
 import com.android.project.github_user_info_task.data.data_class.user_info.UserInfoResp
 import com.android.project.github_user_info_task.data.data_source.user_info.UserInfoDataSource
-import com.android.project.github_user_info_task.data.data_source.user_info.UserInfoRemoteDataSource
 import io.reactivex.rxjava3.core.Single
 
 class GetUserInfoRepository(
